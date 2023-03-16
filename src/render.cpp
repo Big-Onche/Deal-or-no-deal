496caa62786272df0fc938aeb0609057389f116a
CubeConflict <vod-k@live.be>
2023-03-16 07:57:00 +0100

    render | better remaining prices rendering

--- src/render.cpp
+++ src/render.cpp
@@ -20,18 +20,17 @@ namespace render
 
     void drawBoxTop(int id, int playerBox, bool opened, int insideBox) // draw closed or opened top of the box
     {
-        if(!opened) printf("%s%c%s%c\033[0m ", boxcolor(id, playerBox, opened), 218, "------------", 191);
-        else printf("%s%s\033[0m ", boxcolor(id, playerBox, opened), openedBoxTop(insideBox).c_str());
+        if(!opened) printf("%s%c%s%c\033[0m   ", boxcolor(id, playerBox, opened), 218, "------------", 191);
+        else printf("%s%s\033[0m   ", boxcolor(id, playerBox, opened), openedBoxTop(insideBox).c_str());
     }
 
     void drawBoxBottom(int id, int playerBox, bool opened) // bottom of the box with centered box number
     {
-        printf("%s|_____%s%d_____|\033[0m ", boxcolor(id, playerBox, opened), id + 1 > 9 ? "" : "_", id + 1);
+        printf("%s|_____%s%d_____|\033[0m   ", boxcolor(id, playerBox, opened), id + 1 > 9 ? "" : "_", id + 1);
     }
 
     void drawBoxes(const game::playerinfo &player, game::box boxes[]) // rendering all boxes
     {
-        printf("\n");
         loopi(4)
         {
             loopj(4) { // top
@@ -39,43 +38,40 @@ namespace render
                 drawBoxTop(id, player.playerBox, boxes[id].opened, boxes[id].insideBox);
             }
             printf("\n");
+
             loopj(4) { // middle (always the same)
                 int id = i * 4 + j;
-                printf("%s%c%s%c\033[0m ", boxcolor(id, player.playerBox, boxes[id].opened), 218, "------------", 191);
+                printf("%s%c%s%c\033[0m   ", boxcolor(id, player.playerBox, boxes[id].opened), 218, "------------", 191);
             }
             printf("\n");
             loopj(4) { // bottom
                 int id = i * 4 + j;
                 drawBoxBottom(id, player.playerBox, boxes[id].opened);
             }
-            printf("\n\n");
+            printf("\n\n\n");
         }
     }
 
-    void drawRemainingPrices(const game::playerinfo &player, game::box boxes[])
+    void drawRemainingPrices(const game::playerinfo &player, game::box boxes[]) // rendering all remaining prices
     {
+        conoutf(C_DEF, C_DEF, "Remaining boxes: %d\n", game::openCount(true));
+
         int values[game::maxBoxes];
-        int numValues = 0; // keep track of number of non-zero values
+        int numValues = 0;
 
         loopi(game::maxBoxes)
         {
-            if(!boxes[i].opened && boxes[i].insideBox != 0)
-            {
-                values[numValues++] = boxes[i].insideBox;
-            }
+            if (!boxes[i].opened) values[numValues++] = boxes[i].insideBox;
         }
 
-        sort(values, values + numValues); // sort only non-zero values
+        sort(values, values + numValues);
 
-        if(numValues > 0)
-        {
-            conoutf(C_DEF, C_DEF, "Remaining boxes: %d\n", game::openCount(true));
-            loopi(numValues)
-            {
-                conoutf(C_WHITE, values[i]==69 ? C_MAGENTA : values[i]<1000 ? C_BLUE : values[i]<20000 ? C_YELLOW : C_RED, "%d$", values[i]);
-                if(i < numValues - 1) conoutf(C_DEF, C_DEF, " ");
+        for (int i = 0; i < numValues; i += 8) {
+            for (int j = i; j < i + 8 && j < numValues; j++) {
+                int val = values[j];
+                conoutf(C_WHITE, values[j]==69 ? C_MAGENTA : values[j]<2000 ? C_BLUE : values[j]<50000 ? C_YELLOW : C_RED, "$%-11d\033[0m ", val);
             }
-            conoutf(C_DEF, C_DEF, "\n\n");
+            printf("\n\n");
         }
     }
 }
